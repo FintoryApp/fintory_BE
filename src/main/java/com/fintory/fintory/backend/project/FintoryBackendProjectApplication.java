161ad5da4c143a110dfeaaa4195fc0726bc38226
andy <yuniugi1234@gmail.com>
2025-05-10 19:05:01 +0900

    feat: init project

---
+++ src/main/java/com/fintory/fintory/backend/project/FintoryBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fintory.fintory.backend.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintoryBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintoryBackendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fintory/fintory/backend/project/FintoryBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintory.fintory.backend.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintoryBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
