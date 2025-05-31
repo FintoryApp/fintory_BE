41ff76065ce33ba8be118afb57ee1abf384ca972
andy <yuniugi1234@gmail.com>
2025-06-01 01:29:48 +0900

    refactor: 패키지명 및 그룹명 변경

--- src/main/java/com/campuspick/fintory/FintoryBackendProjectApplication.java
+++ src/main/java/com/campuspick/fintory/FintoryBackendProjectApplication.java
@@ -1,4 +1,4 @@
-package com.fintory.fintory.backend.project;
+package com.campuspick.fintory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/campuspick/fintory/FintoryBackendProjectApplicationTests.java
+++ src/test/java/com/campuspick/fintory/FintoryBackendProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fintory.fintory.backend.project;
+package com.campuspick.fintory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
