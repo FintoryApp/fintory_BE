d30efa17d217d32293ebb42393198cb8af7580aa
andy <ugiuk00@naver.com>
2025-07-26 21:22:27 +0900

    feat: 셀레니움+jsoup을 이용하여 뉴스 기사 크롤링 기능 구현

--- domain/src/main/java/com/fintory/domain/news/model/News.java
+++ domain/src/main/java/com/fintory/domain/news/model/News.java
@@ -1,23 +1,40 @@
 package com.fintory.domain.news.model;
 
 import com.fintory.domain.common.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "news")
 @Getter
+@Table(name = "news")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class News extends BaseEntity {
 
     private String title;
 
-    private String content;
+    // 한 기사의 여러 문단을 공백을 구분으로 하는 긴 문자열로 한번에 저장
+    @Column(columnDefinition = "TEXT")
+    private String contents;
+
+    @Column(name="image_url")
+    private String imageUrl;
 
     private String publisher;
 
-    @Column(name="thumbnail_image_url")
-    private String thumbnailImageUrl;
+    @Column(name="published_at")
+    private String publishedAt;
+
+    @Builder
+    private News(String title, String contents, String imageUrl, String publisher, String publishedAt) {
+        this.title = title;
+        this.contents = contents;
+        this.imageUrl = imageUrl;
+        this.publisher = publisher;
+        this.publishedAt = publishedAt;
+    }
 }
