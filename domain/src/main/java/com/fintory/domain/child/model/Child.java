167e631cfb94dbaea2db1cf26720aadb3d583511
andy <ugiuk00@naver.com>
2025-08-07 21:43:23 +0900

    feat: 계정 관련 feature 위해 child 엔티티 구조 수정

--- domain/src/main/java/com/fintory/domain/child/model/Child.java
+++ domain/src/main/java/com/fintory/domain/child/model/Child.java
@@ -3,17 +3,15 @@ package com.fintory.domain.child.model;
 import com.fintory.domain.account.model.Account;
 import com.fintory.domain.alarm.model.Alarm;
 import com.fintory.domain.attendence.model.VisitLog;
-import com.fintory.domain.mapping.MyQuiz;
-import com.fintory.domain.mapping.UserBadge;
 import com.fintory.domain.challenge.model.Challenge;
 import com.fintory.domain.common.BaseEntity;
+import com.fintory.domain.common.Role;
+import com.fintory.domain.mapping.MyQuiz;
 import com.fintory.domain.mapping.ParentChildMapping;
 import com.fintory.domain.point.model.Point;
 import com.fintory.domain.quiz.model.DailyQuizAttempt;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
@@ -23,37 +21,78 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Child extends BaseEntity {
 
+    @Column(length = 20)
     private String nickname;
 
-    private int age;
-
-    @Column(name="login_id")
-    private String loginId;
-
-    private String password;
-
+    @Column(unique = true, nullable = false)
     private String email;
 
-    private boolean status;
+    @Column(nullable = false)
+    private String password;
 
-    //연관관계
-    @OneToOne(cascade = CascadeType.ALL, mappedBy="child")
-    private Account account;
+    // 시큐리티에서 자격 검사할때 필요해서 추가(인증에는 필요없지만, 인가에 필요)
+    @Setter
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 
+    //social login column
+    @Column(name="social_id")
+    private String socialId;
+
+    @Column(name="social_type")
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    // id&pw 생성자
+    @Builder
+    public Child(String nickname, String email, String password, Role role, Status status) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.status = status;
+    }
+    // social 생성자
+    @Builder
+    public Child(String nickname, String email, String socialId, SocialType socialType, Status status) {
+        this.nickname = nickname;
+        this.email = email;
+        this.socialId = socialId;
+        this.socialType = socialType;
+        this.status = status;
+    }
+    // id&pw 방식으로 계정 생성후 social 로그인 추가하는 setter method
+    public void updateSocialInfo (String socialId, SocialType socialType) {
+        this.socialId = socialId;
+        this.socialType = socialType;
+    }
+
+    public void updateStatus(Status status) {
+        this.status = status;
+    }
+
+
+    // n:m
     @OneToOne(cascade = CascadeType.ALL, mappedBy="child")
     private ParentChildMapping parentChildMapping;
-
-    @OneToMany(cascade = CascadeType.ALL,mappedBy="child")
-    private List<Point> point;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "child")
     private List<MyQuiz> myQuizs;
 
+    // 1:1
+    @OneToOne(cascade = CascadeType.ALL, mappedBy="child")
+    private Account account;
+
+    // 1:n
+    @OneToMany(cascade = CascadeType.ALL,mappedBy="child")
+    private List<Point> point;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "child")
     private List<DailyQuizAttempt> dailyQuizzAttempts;
-
-    @OneToMany(cascade = CascadeType.ALL, mappedBy="child")
-    private List<UserBadge> userBadge;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy="child")
     private List<Challenge> challenges;
@@ -63,4 +102,5 @@ public class Child extends BaseEntity {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy="child")
     private List<VisitLog> visitLogs;
+
 }
