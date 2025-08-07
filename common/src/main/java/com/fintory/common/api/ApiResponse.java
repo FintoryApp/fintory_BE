dc1060901ddc01beeb9049e8e7ce673a6b1abc20
andy <ugiuk00@naver.com>
2025-08-07 21:33:59 +0900

    chore: 메세지도 받을 수 있는 응답 생성자 추가

--- common/src/main/java/com/fintory/common/api/ApiResponse.java
+++ common/src/main/java/com/fintory/common/api/ApiResponse.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import static com.fintory.common.api.ApiResultCode.SUCCESS;
-
 @Getter
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 @AllArgsConstructor(access = lombok.AccessLevel.PROTECTED)
@@ -13,13 +11,18 @@ public class ApiResponse<T> {
 
     private ApiResultCode resultCode;
     private T data;
+    private String message;
 
     public static <T> ApiResponse<T> ok(T data) {
-        return new ApiResponse<>(SUCCESS, data);
+        return new ApiResponse<>(ApiResultCode.SUCCESS, data, null);
     }
 
-    public static <T> ApiResponse<T> okWithNoData(T data) {
-        return new ApiResponse<>(SUCCESS, null);
+    public static <T> ApiResponse<T> ok(String message) {
+        return new ApiResponse<>(ApiResultCode.SUCCESS, null, message);
+    }
+
+    public static <T> ApiResponse<T> ok(T data, String message) {
+        return new ApiResponse<>(ApiResultCode.SUCCESS, data, message);
     }
 
 }
