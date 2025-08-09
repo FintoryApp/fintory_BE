d0e0fa3e9a34cc61642ac8baa16d2cf49df37d70
mhee167 <mhee116677@gmail.com>
2025-08-09 11:58:00 +0900

    Revert "feat : 해외 주식 순위 기능 구현"
    This reverts commit 83001ae3375b3e8369e1923a1dc6893000b7fcf8.

--- domain/src/main/java/com/fintory/domain/stock/dto/overseas/wrapper/OverseasStockRankDataWrapper.java
+++
@@ -1,8 +0,0 @@
-package com.fintory.domain.stock.dto.overseas.wrapper;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fintory.domain.stock.dto.overseas.core.OverseasStockRankData;
-
-public record OverseasStockRankDataWrapper(
-    @JsonProperty("output") OverseasStockRankData output
-){}
