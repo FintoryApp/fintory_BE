bfd8aa6e297113b8767eaa66fb6f4121f865f05a
mhee167 <mhee116677@gmail.com>
2025-08-30 01:21:26 +0900

    refactor : 어노테이션 수정

--- app-child/src/main/java/com/fintory/child/domain/trading/TradingController.java
+++ app-child/src/main/java/com/fintory/child/domain/trading/TradingController.java
@@ -5,6 +5,7 @@ import com.fintory.domain.portfolio.dto.TradeRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,7 @@ public interface TradingController {
     @Operation(summary="주식 거래 처리", description="주식 매수 또는 매도 거래를 처리합니다")
     @ApiResponse(responseCode = "200",description="주식 거래 처리 성공")
     public ResponseEntity<com.fintory.common.api.ApiResponse<Void>> trade(
-            @RequestBody TradeRequest tradeRequest,
+            @Valid @RequestBody TradeRequest tradeRequest,
             @AuthenticationPrincipal CustomUserDetails customUserDetails
             );
 
