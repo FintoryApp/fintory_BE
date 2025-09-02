71bc22c44b322052cbb6a47f8a8b171a58e0bce1
andy <ugiuk00@naver.com>
2025-09-02 20:28:59 +0900

    fix: selenium 오류 디버깅

--- infra/src/main/java/com/fintory/infra/selenium/SeleniumConfig.java
+++ infra/src/main/java/com/fintory/infra/selenium/SeleniumConfig.java
@@ -1,37 +1,35 @@
 package com.fintory.infra.selenium;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
-import java.util.UUID;
 
 @Configuration
 public class SeleniumConfig {
 
     @Bean
     public WebDriver chromeDriver() {
-
-        WebDriverManager.chromedriver().setup();
-
         ChromeOptions options = new ChromeOptions();
+        options.setCapability("browserName", "chrome");
 
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
+        RemoteWebDriver driver = null;
+        try {
+            driver = new RemoteWebDriver(
+                    new URL("http://selenium:4444/wd/hub"),
+                    options
+            );
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Invalid Selenium URL", e);
+        }
 
-        String userDataDir = "/tmp/chrome-user-data-" + UUID.randomUUID();
-        new File(userDataDir).mkdirs();
-        options.addArguments("--user-data-dir=" + userDataDir);
-
-        return new ChromeDriver(options);
+        return driver;
     }
 
     @Bean
