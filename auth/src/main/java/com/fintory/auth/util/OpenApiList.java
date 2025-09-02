835cd5f1740215a8b7a82e217e6eb15aa5384c2e
andy <ugiuk00@naver.com>
2025-09-02 21:39:30 +0900

    chore: 시큐리티 설정 변경

--- auth/src/main/java/com/fintory/auth/util/OpenApiList.java
+++ auth/src/main/java/com/fintory/auth/util/OpenApiList.java
@@ -24,6 +24,8 @@ public class OpenApiList {
             "/swagger-ui/**",
             "/swagger-ui.html",
             "/swagger-ui/index.html",
+            "/swagger-resources/**",
+            "/webjars/**",
             "/v3/api-docs",
             "/v3/api-docs/**",
 
