1b068dbfb5d4c037db92acb4feb5ca9c841c530b
andy <ugiuk00@naver.com>
2025-09-02 20:48:22 +0900

    fix: selenium 오류 디버깅

--- infra/src/main/java/com/fintory/infra/selenium/SeleniumConfig.java
+++ infra/src/main/java/com/fintory/infra/selenium/SeleniumConfig.java
@@ -17,7 +17,7 @@ public class SeleniumConfig {
     @Bean
     public WebDriver chromeDriver() throws MalformedURLException {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
+        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
