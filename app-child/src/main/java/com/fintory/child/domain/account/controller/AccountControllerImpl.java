6bbc7c61e8a776fb2bfd73cc3ef906359e75fcc6
andy <ugiuk00@naver.com>
2025-09-09 21:11:22 +0900

    fix: 누락 어노테이션 추가. localdatetime 직렬화 위해 objectmapper 설정 추가.

--- app-child/src/main/java/com/fintory/child/domain/account/controller/AccountControllerImpl.java
+++ app-child/src/main/java/com/fintory/child/domain/account/controller/AccountControllerImpl.java
@@ -11,11 +11,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("/api/child/account")
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/child/account")
 public class AccountControllerImpl implements AccountController{
 
     private final AccountService accountService;
