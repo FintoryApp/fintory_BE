a3a477f91a5d1d21613a8abf63146a454435febe
mhee167 <mhee116677@gmail.com>
2025-09-16 11:50:56 +0900

    refactor : 구조 변경으로 인한 파일 삭제

--- app-child/src/main/java/com/fintory/child/portfolio/service/StockTransactionService.java
+++
@@ -1,9 +0,0 @@
-package com.fintory.child.portfolio.service;
-
-import com.fintory.domain.portfolio.model.StockTransaction;
-
-import java.util.List;
-
-public interface StockTransactionService {
-    public List<StockTransaction> getStockTransactions();
-}
