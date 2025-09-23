b89b6582dc15746bd9d05ca6e4a8a213eb243da9
andy <ugiuk00@naver.com>
2025-09-23 20:13:51 +0900

    debug: 하트비트 구현위해 TaskScheduler 추가

--- infra/src/main/java/com/fintory/infra/domain/stock/config/WebSocketBrokerConfig.java
+++ infra/src/main/java/com/fintory/infra/domain/stock/config/WebSocketBrokerConfig.java
@@ -1,7 +1,10 @@
 package com.fintory.infra.domain.stock.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -11,6 +14,11 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Bean
+    public TaskScheduler messageBrokerTaskScheduler() {
+        return new ThreadPoolTaskScheduler();
+    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
