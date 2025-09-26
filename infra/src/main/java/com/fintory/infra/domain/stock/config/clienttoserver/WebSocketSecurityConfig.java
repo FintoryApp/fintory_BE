ace528efeabdcde621ecd7a51b2eb495412b3f97
andy <ugiuk00@naver.com>
2025-09-26 14:48:36 +0900

    debug: 임시로 모든 stomp 레벨 요청 허용

--- infra/src/main/java/com/fintory/infra/domain/stock/config/clienttoserver/WebSocketSecurityConfig.java
+++ infra/src/main/java/com/fintory/infra/domain/stock/config/clienttoserver/WebSocketSecurityConfig.java
@@ -3,7 +3,6 @@ package com.fintory.infra.domain.stock.config.clienttoserver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
@@ -16,10 +15,7 @@ public class WebSocketSecurityConfig {
 
     @Bean
     public AuthorizationManager<Message<?>> messageAuthorizationManager(MessageMatcherDelegatingAuthorizationManager.Builder messages) {
-        messages
-                .simpTypeMatchers(SimpMessageType.CONNECT).permitAll()
-                .simpDestMatchers("/topic/**").permitAll()
-                .anyMessage().authenticated();
+        messages.anyMessage().permitAll();
         return messages.build();
     }
 }
