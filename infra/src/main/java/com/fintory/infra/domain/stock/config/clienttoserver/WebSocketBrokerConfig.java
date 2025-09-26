b633404762823f4de8e583d0d9b63d519e4b6f75
andy <ugiuk00@naver.com>
2025-09-26 14:34:48 +0900

    debug: stomp 레벨 인증인가 프로토콜 활성화

--- infra/src/main/java/com/fintory/infra/domain/stock/config/clienttoserver/WebSocketBrokerConfig.java
+++ infra/src/main/java/com/fintory/infra/domain/stock/config/clienttoserver/WebSocketBrokerConfig.java
@@ -1,10 +1,11 @@
-package com.fintory.infra.domain.stock.config;
+package com.fintory.infra.domain.stock.config.clienttoserver;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 
 //클라이언트들이 내 서버에 연결하도록 설정하는 코드
 //REVIEW 일반적인 어플에서도 시세 데이터는 별도의 로그인 과정 없이도 조회가 가능해서 핸드셰이크 인터셉터 설정x(jwt 토큰 인증x)
