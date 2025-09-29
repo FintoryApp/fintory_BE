849954a8e9e8cc0f95cd1503d4f066d0f4d09d6c
mhee167 <mhee116677@gmail.com>
2025-09-30 02:04:03 +0900

    refactor: 테스트 용도로 스케쥴러를 앱 기동 후 무조건 실행되도록 변경

--- infra/src/main/java/com/fintory/infra/domain/stock/service/websocket/LiveStockPriceWebsocketServiceImpl.java
+++ infra/src/main/java/com/fintory/infra/domain/stock/service/websocket/LiveStockPriceWebsocketServiceImpl.java
@@ -14,6 +14,7 @@ import com.fintory.infra.domain.stock.handler.OverseasLiveStockPriceWebSocketHan
 import com.fintory.infra.domain.stock.repository.LiveStockPriceRepository;
 import com.fintory.infra.domain.stock.repository.StockPriceHistoryRepository;
 import com.fintory.infra.domain.stock.repository.StockRepository;
+import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -391,7 +392,8 @@ public class LiveStockPriceWebsocketServiceImpl implements LiveStockPriceWebsock
     }
 
 
-    @Scheduled(cron="0 00 11 * * MON-FRI", zone="America/New_York")
+    //@Scheduled(cron="0 00 11 * * MON-FRI", zone="America/New_York")
+    @PostConstruct
     public void startOverseasMarketSubscription(){
 
         if (!isOverseasMarketOpen()) {
