ba6d20469af901b6f705e577ba6a5fabc2e62e3d
mhee167 <mhee116677@gmail.com>
2025-10-23 21:57:47 +0900

    refactor: test용으로 20개의 child만 검사

--- infra/src/main/java/com/fintory/infra/domain/child/repository/ChildRepository.java
+++ infra/src/main/java/com/fintory/infra/domain/child/repository/ChildRepository.java
@@ -3,6 +3,7 @@ package com.fintory.infra.domain.child.repository;
 import com.fintory.domain.child.model.Child;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChildRepository extends JpaRepository<Child, Long> {
@@ -11,4 +12,5 @@ public interface ChildRepository extends JpaRepository<Child, Long> {
 
     Optional<Child> findBySocialId(String socialId);
 
+    List<Child> findTop20ByOrderByIdAsc();
 }
