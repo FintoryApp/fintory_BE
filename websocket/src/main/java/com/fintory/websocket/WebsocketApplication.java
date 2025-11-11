58fb44c9aa03496fd65c00d0c9973993d6f68535
mhee167 <mhee116677@gmail.com>
2025-11-12 01:12:05 +0900

    fix : @EnableJpaAuditing 추가

--- websocket/src/main/java/com/fintory/websocket/WebsocketApplication.java
+++ websocket/src/main/java/com/fintory/websocket/WebsocketApplication.java
@@ -3,8 +3,7 @@ package com.fintory.websocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -24,6 +23,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
                 "com.fintory.websocket.publisher.repository"
         }
 )
+@EnableJpaAuditing
 public class WebsocketApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebsocketApplication.class, args);
